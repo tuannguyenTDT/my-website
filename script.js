284cfefc68244e35f8a7e7a513bf3e832032ae65
tuanna <nguyen.anh.tuan@sankei-bc.co.jp>
2026-08-10 09:02:38 +0700

    add

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-function sayHello() {
-    alert("Hello GitHub Pages!");
+const openWishButton = document.getElementById("openWish");
+const wish = document.getElementById("wish");
+const fireworks = document.getElementById("fireworks");
+
+openWishButton.addEventListener("click", () => {
+
+    wish.classList.remove("hidden");
+
+    openWishButton.textContent = "🎉 Chúc mừng sinh nhật!";
+
+    createFireworks();
+
+});
+
+
+function createFireworks() {
+
+    for (let i = 0; i < 15; i++) {
+
+        setTimeout(() => {
+
+            const firework = document.createElement("div");
+
+            firework.className = "firework";
+
+            firework.style.left =
+                `${Math.random() * 100}%`;
+
+            firework.style.top =
+                `${Math.random() * 70}%`;
+
+            fireworks.appendChild(firework);
+
+            setTimeout(() => {
+                firework.remove();
+            }, 1000);
+
+        }, i * 120);
+    }
 }
